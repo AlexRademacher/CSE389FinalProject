27c4378927c83ee93ba6beaa18cb4fd68c3c8433
Jamcat42 <jamcatjunk@outlook.com>
2025-12-07 15:56:25 -0500

    Blueprint Spawner

---
+++ Source/CSE389FinalProject/BallSpawner.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BallSpawner.h"
+#include "Components/BoxComponent.h"
+#include "ObstacleBall.h"
+
+// Sets default values
+ABallSpawner::ABallSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	SpawnVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnVolume"));
+	SpawnVolume->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void ABallSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ABallSpawner::SpawnActor, 5.0f, true);
+}
+
+// Called every frame
+void ABallSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABallSpawner::SpawnActor()
+{
+	FVector SpawnLocation = GetActorLocation();
+	FRotator SpawnRotation = GetActorRotation();
+
+
+	GetWorld()->SpawnActor<AActor>(ObstacleBall, GetActorLocation(), GetActorRotation());
+	//GetWorld()->SpawnActor<AObstacleBall>(SpawnLocation, SpawnRotation);
+}
---
+++ Source/CSE389FinalProject/BallSpawner.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BallSpawner.generated.h"
+
+UCLASS()
+class CSE389FINALPROJECT_API ABallSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABallSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* SpawnVolume;
+
+	UPROPERTY(EditAnywhere, Category = "Parameters")
+	TSubclassOf<AActor> ObstacleBall;
+
+	FTimerHandle TimerHandle;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SpawnActor();
+};
