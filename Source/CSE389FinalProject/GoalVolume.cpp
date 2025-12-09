48f5e2c4ad46eb3a51632eea142c34fbacdf94b7
BumbleWeeb <Alex@abreu.net>
2025-12-08 19:39:03 -0500

    Create GoalVolume.cpp

---
+++ Source/CSE389FinalProject/GoalVolume.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoalVolume.h"
+#include "Components/BoxComponent.h"
+#include "Ball.h"
+#include "PlayerCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+AGoalVolume::AGoalVolume()
+{
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = Box;
+
+	Box->SetCollisionProfileName("OverlapAllDynamic");
+	Box->OnComponentBeginOverlap.AddDynamic(this, &AGoalVolume::OnOverlapBegin);
+
+}
+void AGoalVolume::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+    if (OtherActor->IsA(ABall::StaticClass()))
+    {
+
+        APlayerCharacter* Player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+        if (Player)
+        {
+            int32 NewScore = Player->GetScore() + 1;
+            Player->SetScore(NewScore);
+
+            AGameModeBase* GM = GetWorld()->GetAuthGameMode(); 
+            if (GM)
+            {
+                FName FunctionName = "ShowNextLevel"; 
+                UFunction* Function = GM->FindFunction(FunctionName);
+                if (Function)
+                {
+                    GM->ProcessEvent(Function, nullptr); 
+                }
+                else
+                {
+                    UE_LOG(LogTemp, Warning, TEXT("Function %s not found in GameMode!"), *FunctionName.ToString());
+                }
+            }
+        }
+    }
+}
